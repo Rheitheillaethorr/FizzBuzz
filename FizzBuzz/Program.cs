23ebd9accbd10a45c70e56d1b935682fbba5ec84
Rheitheillaethor <minegocu3@gmail.com>
2021-11-09 22:09:40 +0100

    save

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,7 +6,29 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DoFizzBuzz();
+        }
+        static int GettingInfo()
+        {
+            Console.WriteLine("Give me last integer number, you want to check in FizzBuzz");
+            string givenAnswer = Console.ReadLine();
+            try
+            {
+                int givenNumber = int.Parse(givenAnswer);
+                return givenNumber;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("{0} is not an integer", givenAnswer);
+                Console.WriteLine("Try again!");
+                GettingInfo();
+                return 0; //Program.Gettinginfo()': not all code path return a value --> easy xD
+            }
+        }
+        static void DoFizzBuzz()
+        {
+            int x = GettingInfo();
+            Console.WriteLine(x);
         }
     }
 }
